51a59876b4e7a23287680ed14c6dcc79d4c11c5d
Michele <ercolinimichele@outlook.it>
2024-02-16 19:29:00 +0100

    Modello category implementato

---
+++ database/migrations/2024_02_16_161045_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->unsignedBigInteger("product_id");
+            $table->foreign("product_id")->references("id")->on("products");
+            $table->timestamps();
+        });
+
+        $categories = ["Abbigliamento", "Elettronica", "Libri", "Videogiochi"];
+
+        foreach ($categories as $category) {
+            Category::create([
+                "name"=> $category
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
